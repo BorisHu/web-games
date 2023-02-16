45a452a697627146826364ce8e9864b75ddf5fec
BorisHu <91997134+BorisHu@users.noreply.github.com>
2023-02-16 14:50:42 -0800

    Take example and adjust

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
-import {
-    BoxGeometry,
-    Mesh,
-    MeshPhongMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-    DirectionalLight,
-  } from "three";
-  
-  // Create our scene
-  const scene = new Scene();
-  
-  // Create the camera so we can see our scene
-  const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  
-  // Create our renderer and add it to the DOM
-  const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-  
-  // Create our cube mesh from  a geometry and a material and add it to the scene
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshPhongMaterial({ color: 0x00ff00 });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
-  
-  // Add a directional light so we can see shadows on the cube
-  const color = 0xffffff;
-  const intensity = 1;
-  const light = new DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
-  
-  // Position the camera
-  camera.position.z = 5;
-  
-  // The animation loop updates the cube's rotation
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+let coins;
+let player;
+let score = 20;
+function setup() {
+  createCanvas(windowWidth/2, windowHeight).center('horizontal');
+  coins = new Group();
+  for (let i = 0; i < 10; i++) {
+    let c = createSprite(
+      random(100, width-100),
+      random(100, height-100),
+      10, 10);
+    c.shapeColor = color(255, 255, 0);
+    coins.add(c);
   }
-  
-  // Start the animation loop
-  animate();
+  player = createSprite(50, 50, 40, 40);
+  player.shapeColor = color(255);
+}
+function draw() {
+  background(50);
+//   player.position.x = mouseX;
+//   player.position.y = mouseY;
+    player.velocity.x = 
+        (mouseX-player.position.x)*1;
+    player.velocity.y = 
+        (mouseY-player.position.y)*1;
+  player.overlap(coins, getCoin);
+  drawSprites();
+  fill(255);
+  noStroke();
+  textSize(72);
+  textAlign(CENTER, CENTER);
+  if (coins.length > 0) {
+    text(score, width/2, height/2);
+  }
+  else {
+    text("you lose!", width/2, height/2);
+  }
+}
+function getCoin(player, coin) {
+  coin.remove();
+  score -= 1;
+}
